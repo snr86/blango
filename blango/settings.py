b62b648f79cbdd0f8d6927fdbd17926cfcb40f37
Simon Reynolds <simonnreynolds@mac.com>
2022-08-30 18:23:33 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -18,6 +18,8 @@ import dj_database_url
 class Dev(Configuration):
     AUTH_USER_MODEL = "blango_auth.User"  
     
+    ROOT_URLCONF = 'blango.urls'
+
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
     BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,15 +52,26 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
         'debug_toolbar',
-
-        
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
     ]
+
+    SITE_ID = 1
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+
 
     # Cripsy Settings
     CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
@@ -156,6 +169,8 @@ class Dev(Configuration):
 
     ACCOUNT_ACTIVATION_DAYS = 7
 
+   # Django Registration Email
+    # console email backend prints to terminal
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
